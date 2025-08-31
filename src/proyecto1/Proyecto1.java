65eb8a3c3bde74badcf617a3e25ff8dd375261b3
alanjavieru <3798302560101@ingenieria.usac.edu.gt>
2025-08-30 19:07:22 -0600

    Se agrega el proyecto

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,30 @@
+package proyecto1;
+
+import java.util.Scanner;
+
+public class Proyecto1 {
+    
+        private static Scanner teclado = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de inventario");
+     
+        int opcion;
+ 
+        System.out.println("-------- Menú ---------");
+        System.out.println("1. Agregar producto");
+        System.out.println("2. Buscar producto");
+        System.out.println("3. Eliminar producto"); 
+        System.out.println("4. Registrar venta");
+        System.out.println("5. Bitácora");
+        System.out.println("6. Generar reportes");
+        System.out.println("7. Ver datos de estudiante");
+        System.out.println("8. Salir");
+    
+        System.out.println("Seleccione una opción");
+    
+        }
+    
+    }
+   
